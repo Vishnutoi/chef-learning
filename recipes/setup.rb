09e81740e4afa1e2de7eba6e501a58257acfdfd2
Cloud User <centos@ip-172-31-21-51.ec2.internal>
2021-03-28 15:45:22 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'ntp' do
+    action :install
+end
+
+package 'tree' do
+    action :install
+end
+
+package 'git' do
+    action :install
+end
+
+
+file '/etc/motd' do
+    content 'This server is a property of vishnu '
+    owner 'root'
+    group 'root'
+    mode '0755'
+    action :create
+end
+
+
